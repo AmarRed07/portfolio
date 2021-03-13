7e1c3383861a281212658a004f596d48661d76a8
AmarRed07 <redzepagicamar@yahoo.com>
2021-03-12 17:46:03 -0800

    services

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,11 +11,14 @@ const AboutMe = () => {
                     </div>
                 </div>
                 <div className="col-lg-6 col-xm-12">
-                    <h1 className="about-heading">about me</h1>
+                    <h1 className="about-heading">A little about me:</h1>
                     <p>
-                        A little about me. I am a bubs living in a bubs world. I am married to the best
-                        momma nuggy in the world. And, I have 3 little bub nuggies. Mr. Maygi, Poesopher, and
-                        Gainz (aka fuck bitches get muney).
+                        I’m a Software Developer/Engineer with passions to explore and formularize myself with the ever-growing technologies that come with programming territory. 
+                        My passions give me the ability to continue learning – daily – while polishing my coding skills in all aspects of Software Development. I am currently fluent in 
+                        JavaScript, HTML/CSS, and React. My projects are new, but many. I’ve always been quick to catch on and accomplish my goals and passions in life. Though I consider 
+                        myself a “Jr.” I will not be limited with challenges presented too me with projects that a potential client or employer might present. If it is the first time encountering
+                        a given problem during development, it will not hinder my performance. I am quick to learn and provide myself with the necessary knowledge and tools to finish the job - not only in 
+                        decent time - but with focus and care provided by personal touch that is guaranteed to provide satification to all clients and employers.
                     </p>
                 </div>
             </div>
---
+++ src/components/Services.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
+
+const Services = () => {
+    return (
+    <div className="services">
+        <h1 className="py-5">Skills & Services</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 col-sm-6">
+                        <div className="box">
+<div className="circle"><FontAwesomeIcon className="icon" icon={faDesktop} size="2x" /></div>
+                           <h3 className="title">Web Design</h3>
+                            <p>I approach each porject individually and always with focus on the reslts.</p>
+                        </div>
+                    </div>
+                    {/* - */}
+                    <div className="col-md-3 col-sm-6">
+                        <div className="box">
+<div className="circle"><FontAwesomeIcon className="icon" icon={faFileCode} size="2x" /></div>
+
+                           <h3>Web Developer</h3>
+                           <p>Your website will be built with extreme personal effort and creativity.</p>
+                        </div>
+                    </div>
+                    {/* - */}
+                    <div className="col-md-3 col-sm-6">
+                        <div className="box">
+<div className="circle"><FontAwesomeIcon className="icon" icon={faFacebookF} size="2x" /></div>
+
+                           <h3>FrontEnd-Specialist</h3>
+                           <p>Feel comfort and ease knowing that your projects are being handled by a professional.</p>
+                        </div>
+                    </div>
+                    {/* - */}
+                    <div className="col-md-3 col-sm-6">
+                        <div className="box">
+<div className="circle"><FontAwesomeIcon className="icon" icon={faGoogle} size="2x" /></div>
+
+                           <h3>Google Priorities</h3>
+                           <p>JavaScript development & HTML5+/CSS web customization. Web applications created by React!
+                            Have your website show as top results during Google searches. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
